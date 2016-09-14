a530da797453102d6ba822470a838eeb15a6ad3a
aquasi <drickzee@gmail.com>
2016-09-14 14:47:33 -0400

    First push

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,88 @@
+package com.example.android.miwok;
+
+/**
+ * Created by kay on 9/8/16.
+ */
+public class Word {
+
+    /**
+     * Default translation for the word
+     */
+    private String mDefaultTranslation;
+
+
+
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
+
+    /**
+     * Image resource ID for the word
+     */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /**
+     * Constant value that represents no image was provided for this word
+     */
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+
+    /**
+     * Miwok translation for the word
+     */
+    private String mMiwokTranslation;
+
+
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation   is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation, int ImageResourceId, int audioResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = ImageResourceId;
+        mAudioResourceId = audioResourceId;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+    /**
+     * @return image resource ID
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+
+    /* Plays audio is there's one*/
+    public int getAudioResourceId() {
+        return mAudioResourceId;
+    }
+}
